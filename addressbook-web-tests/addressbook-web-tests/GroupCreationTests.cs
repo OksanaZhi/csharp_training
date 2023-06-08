07e7a208dca921ed151f1292d1c0a7eb1dd6b200
joa@rio-soft.ru <joa@rio-soft.ru>
2023-06-08 17:45:46 +0500

    Задание 2

--- addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.UI; 
 
 namespace WebAaddressbookTests
 {
