e5f34e6ecb8b35d717c596541c634e169b9aa9bf
joa@rio-soft.ru <joa@rio-soft.ru>
2023-08-14 16:49:52 +0500

    лекция7_2

--- addressbook-web-tests/addressbook-web-tests/tests/GroupRemovalTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupRemovalTests.cs
@@ -12,7 +12,7 @@ using LinqToDB;
 namespace WebAddressbookTests
 {
     [TestFixture]
-    public class GroupRemovalTests : AuthTestBase
+    public class GroupRemovalTests : GroupTestBase
     {
        
         [Test]
